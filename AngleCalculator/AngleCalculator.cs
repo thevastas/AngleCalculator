b59ebe57d2df5fc2912215bf13db2dd1fcc6ae1d
vastas <v.astasauskas@gmail.com>
2022-05-30 17:07:29 +0300

    First hardcoded version

---
+++ AngleCalculator/AngleCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class AngleCalculator
+    {
+        public int PosToDegrees(int arrowpos, bool IsHour)
+        {
+            int angle;
+
+            if (IsHour)
+            {
+                if (arrowpos < 0 || arrowpos > 24) throw new System.ArithmeticException();
+                else if (arrowpos > 12) arrowpos -= 12;
+                angle = 360 * arrowpos / 12;
+            }
+            else angle = 360 * arrowpos / 60;
+
+            return angle;
+        }
+
+        public int CalculateAngle(int number1, int number2)
+        {
+            int anglehours = PosToDegrees(number1, true);
+            int angleminutes = PosToDegrees(number2, false);
+            
+            var anglevalue = Math.Abs(anglehours - angleminutes);
+        if (anglevalue > 180) anglevalue = 360 - anglevalue;
+
+            return anglevalue;
+        }
+    }
--- AngleCalculator/Program.cs
+++ AngleCalculator/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿AngleCalculator instance1 = new AngleCalculator();
+Console.WriteLine("Enter the time in the following format: HH:mm (24H)");
+Console.WriteLine(instance1.CalculateAngle(0, 31));
