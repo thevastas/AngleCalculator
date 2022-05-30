f3771a965fd9333736f96528f0cc9ed5daec0864
vastas <v.astasauskas@gmail.com>
2022-05-30 21:45:55 +0300

    Implemented the degree difference for the hour arrow due to minutes arrow position

--- AngleCalculator/AngleCalculator.cs
+++ AngleCalculator/AngleCalculator.cs
@@ -5,31 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-    public class AngleCalculator
+public class AngleCalculator
+{
+
+    static int MinutesDegrees(int minutespos)
     {
-        public int PosToDegrees(int arrowpos, bool IsHour)
-        {
-            int angle;
+        int angle;
+        if (minutespos < 0 || minutespos > 59) throw new System.ArithmeticException();
+        angle = 360 * minutespos / 60;
+        return angle;
+    }
 
-            if (IsHour)
-            {
-                if (arrowpos < 0 || arrowpos > 24) throw new System.ArithmeticException();
-                else if (arrowpos > 12) arrowpos -= 12;
-                angle = 360 * arrowpos / 12;
-            }
-            else angle = 360 * arrowpos / 60;
+    static int HoursDegrees(int hourspos, int minutespos)
+    {
+        int angle;
+        if (hourspos < 0 || hourspos > 24) throw new System.ArithmeticException();
+        else if (hourspos > 12) hourspos -= 12;
+        angle = 360 * hourspos / 12;
+        angle += (minutespos / 2);
+        return angle;
+    }
 
-            return angle;
-        }
+    public int CalculateAngle(int hours, int minutes)
+    {
 
-        public int CalculateAngle(int number1, int number2)
-        {
-            int anglehours = PosToDegrees(number1, true);
-            int angleminutes = PosToDegrees(number2, false);
-            
-            var anglevalue = Math.Abs(anglehours - angleminutes);
+        int angleminutes = MinutesDegrees(minutes);
+        int anglehours = HoursDegrees(hours, minutes);
+
+        var anglevalue = Math.Abs(anglehours - angleminutes);
         if (anglevalue > 180) anglevalue = 360 - anglevalue;
 
-            return anglevalue;
-        }
+        return anglevalue;
     }
+}
--- AngleCalculator/Program.cs
+++ AngleCalculator/Program.cs
@@ -1,3 +1,24 @@
-﻿AngleCalculator instance1 = new AngleCalculator();
+﻿AngleCalculator anglecalculator = new AngleCalculator();
+
 Console.WriteLine("Enter the time in the following format: HH:mm (24H)");
-Console.WriteLine(instance1.CalculateAngle(0, 31));
+
+string line = Console.ReadLine();
+string[] words = line.Split(':');
+
+try
+{
+    int angle = anglecalculator.CalculateAngle(Int16.Parse(words[0]), Int16.Parse(words[1]));
+    Console.WriteLine($"The angle between the hour and the minute arrow:\t{angle,8:G} degrees");
+}
+catch (System.ArithmeticException)
+{
+    Console.WriteLine("The format of the time is invalid");
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("The entered value is not a number");
+}
+
+
+
+
